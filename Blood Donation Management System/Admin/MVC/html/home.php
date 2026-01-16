3ade73b0d7be87fc91f029fdfad70ed86fca61a4
Ashrafulhossain2002 <asrafulhossain2002@gmail.com>
2026-01-17 04:23:07 +0600

    home

---
+++ Blood Donation Management System/Admin/MVC/html/home.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header("Location: login.php");
+    exit();
+}
+
+$user = $_SESSION['user'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Home | Blood Donation</title>
+<link rel="stylesheet" href="home.css">
+</head>
+<body>
+
+<header>
+<h1>Blood Donation Dashboard</h1>
+<p>Welcome, <?= $user['name'] ?></p>
+<a href="logout.php" class="logout">Logout</a>
+</header>
+
+<div class="cards">
+    <div class="card donor">
+        <h3>Donors</h3>
+        <p>Registered blood donors</p>
+    </div>
+    <div class="card recipient">
+        <h3>Recipients</h3>
+        <p>Blood receivers</p>
+    </div>
+    <div class="card Admin">
+        <h3>Admin</h3>
+        <p>Admin</p>
+    </div>
+</div>
+<footer>
+    <div>
+        <p>Copyright & Disclaimer
+© 2026 [Blood Management system]. All Rights Reserved. All content, including donor data, inventory records, and application code, is the property of [Name]. The information on this platform is for blood management and donation purposes only. We do not sell, share, or trade personal donor information.</p>
+    </div>
+</footer>
+
+</body>
+</html>
