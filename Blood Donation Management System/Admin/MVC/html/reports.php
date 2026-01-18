b37b0e3094bd50d3e1a40b6777013ed4877e93fb
Ashrafulhossain2002 <asrafulhossain2002@gmail.com>
2026-01-19 02:29:01 +0600

    php

---
+++ Blood Donation Management System/Admin/MVC/html/reports.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Reports</title>
+<link rel="stylesheet" href="reports.css">
+</head>
+<body>
+
+<h2>Reports</h2>
+<ul>
+<li>Total Donors: 15</li>
+<li>Blood Group O+: 5</li>
+<li>Blood Group A+: 4</li>
+<li>Blood Group B-: 2</li>
+<li>Blood Group A-: 2</li>
+<li> Blood Group AB-:1</li>
+<li>Blood Group O-:1</li>
+</ul>
+
+<a href="admin-dashboard.php">Back</a>
+<footer>
+    
+
+</body>
+<footer>
+    <div>
+        <p>Copyright & Disclaimer
+© 2026 [Blood Management system]. All Rights Reserved. All content, including donor data, inventory records, and application code, is the property of [Name]. The information on this platform is for blood management and donation purposes only. We do not sell, share, or trade personal donor information.</p>
+    </div>
+</footer>
+</html>
