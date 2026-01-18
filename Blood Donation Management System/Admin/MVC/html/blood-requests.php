4064d20071c865ff518c23a0b9fcbe24aac93bd0
Ashrafulhossain2002 <asrafulhossain2002@gmail.com>
2026-01-19 02:10:52 +0600

    php

---
+++ Blood Donation Management System/Admin/MVC/html/blood-requests.php
@@ -0,0 +1,30 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blood Requests</title>
+<link rel="stylesheet" href="blood-requests.css">
+</head>
+<body>
+
+<h2>Blood Requests</h2>
+<ul>
+<li>O+ needed – Dhaka</li>
+<li>A- needed – Chittagong</li>
+<li>A+ needed – Dhaka</li>
+<li>B- needed – Rajshahi</li>
+<li>O- needed – Dhaka</li>
+<li>AB- needed – Naraygonj</li>
+</ul>
+
+<a href="admin-dashboard.php">Back</a>
+
+<footer>
+    <div>
+        <p>Copyright & Disclaimer
+© 2026 [Blood Management system]. All Rights Reserved. All content, including donor data, inventory records, and application code, is the property of [Name]. The information on this platform is for blood management and donation purposes only. We do not sell, share, or trade personal donor information.</p>
+    </div>
+</footer>
+
+</body>
+</html>
